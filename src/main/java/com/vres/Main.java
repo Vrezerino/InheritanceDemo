36068d070f121099eeb21c7c8f84e1a742b82132
Vrezerino <parkpatrick1@gmail.com>
2022-09-21 17:03:56 +0300

    empty main method

--- src/main/java/com/vres/Main.java
+++ src/main/java/com/vres/Main.java
@@ -3,11 +3,6 @@ package com.vres;
 public class Main {
 
 	public static void main(String[] args) {
-		RegularCustomer michael = new RegularCustomer("Michael", 0, 20);
-		System.out.println(michael.sound());
-
-		VIPCustomer patrick = new VIPCustomer("Patrick", 100000, 5);
-		System.out.println(patrick.sound());
-		patrick.walk();
+		// See CustomerTest.java
 	}
 }
